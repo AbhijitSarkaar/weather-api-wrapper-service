59bb36cab716e46e08a7c4ee12c502dc7d0f3824
AbhijitSarkaar <abhijit.sarkaar26@gmail.com>
2025-03-04 16:07:10 +0530

    redis production setup

--- utils/redis-client.js
+++ utils/redis-client.js
@@ -1,8 +1,16 @@
+require("dotenv").config();
 const redis = require("redis");
 
 const redisClient = redis.createClient({
-  port: 6379,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
 });
+
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
 
 redisClient.connect().then(() => {
   console.log("redis client connected");
